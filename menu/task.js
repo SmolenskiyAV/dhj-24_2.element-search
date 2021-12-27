43d3aa06fd864b4429055f559a698fa570aad32e
SmolenskiyAV <JoJo_666@list.ru>
2021-12-28 00:50:54 +0700

    Версия 1.2 Задание 2 подкорректировано

--- menu/task.js
+++ menu/task.js
@@ -1,50 +1,43 @@
 // ### Task 2.2 ###
 
-const menuLink = document.getElementsByClassName("menu__link"); // поиск коллекции с пунктами меню
-//const menuLinkArray = [...menuLink];                            // преобразовываем коллецию menuLink в массив объектов
+const submenuLink = document.querySelectorAll('.menu_sub'); // поиск коллекции с пунктами подменю
 
    let openedSubMenu;    // переменная, для хранения текущего открытого подменю
 
-   for (i = 0; i < menuLink.length; i++) {   // присвоение onclick-события элементам menuLink через цикл       
+   function menuOperate(){
+      
+      if (openedSubMenu != null) {        // если в меню уже есть откытые подменю
+      openedSubMenu.classList.remove('menu_active');   // закрываем открытое подменю
+      openedSubMenu = null;   // откытых подменю больше нет
+      }
+     
+      this.querySelector('.menu_sub').classList.add('menu_active');   // открываем выпадающее подменю
+      openedSubMenu = this.querySelector('.menu_sub');                // фиксируем факт наличия открытого подменю
+                 
+      return false;                                  // Запрещаем переход по ссылке для тех пунктов, что имеют вложенное подменю
+   };
+
+   for (let i = 0; i < submenuLink.length; i++) {          
     
-      (function(n) {
-       menuLink[n].onclick = function (){
+      submenuLink[i].closest('li').onclick = menuOperate;  // присвоение через цикл onclick-события элементам меню, содержащим подменю 
+      
+   };
 
-         if (openedSubMenu != null) {        // если в меню уже есть откытые подменю
-         openedSubMenu.classList.remove('menu_active');   // закрываем открытое подменю
-         openedSubMenu = null;   // откытых подменю больше нет
-         };
-        
-         if (menuLink[n].closest('li') != null) {  // если элемент, на котором зафиксирован клик, вложен в элемент списка <li>
 
-            const selectedPunkt = menuLink[n].closest('li'); // выбранный элемент <li>
-            
-            if (selectedPunkt.querySelector('ul.menu_sub')) { // если перебираемый(вложенный) элемент списка содержит элемент с классом "menu_sub"
-               selectedPunkt.querySelector('ul.menu_sub').classList.add('menu_active');   // открываем выпадающее меню
-               openedSubMenu = selectedPunkt.querySelector('ul.menu_sub');
-               return false;                                  // Запрещаем переход по ссылке для тех пунктов, что имеют вложенное подменю
-            };
-            
-         }; 
-      };
-      }(i));
-   }
-
-   const d = document.querySelectorAll('div', 'p', 'span'); // формирование коллекции элементов, находящихся на странице, но не содержащей элемента меню <ul>. Предполагается перечеслине всех этих элементов в качестве параметров querySelectorAll. Как без цикла одной строкой вывести всю коллекцию страницы, но без элементов меню <ul> по-другому??? Может есть фильтр для этого метода, в котором можно указать: "выводить всё, кроме <ul>"???
+   const notMunuElement = document.querySelectorAll('div', 'p', 'span'); // формирование коллекции элементов, находящихся на странице, но не содержащей элемента меню <ul>. Предполагается перечеслине всех этих элементов в качестве параметров querySelectorAll. Как без цикла одной строкой вывести всю коллекцию страницы, но без элементов меню <ul> по-другому??? Может есть фильтр для этого метода, в котором можно указать: "выводить всё, кроме <ul>"???
  
-   console.log(d);
+   
+   for (m =0; m < notMunuElement.length; m++) {
 
-   for (m =0; m < d.length; m++) {
-
-      (function(m) {
-         d[m].onclick = function (){ // обработка клика на любом элементе страницы, кроме меню (чтобы сворачивать уже раскрытое подменю, если клик происходит за пределами меню)
-         if ((openedSubMenu != null)) {        // если в меню уже есть откытые подменю, и клик происходит за пределами меню
+      notMunuElement[m].onclick = function (){ // обработка клика на любом элементе страницы, кроме меню (чтобы сворачивать уже раскрытое подменю, если клик происходит за пределами меню)
+      
+      if ((openedSubMenu != null)) {        // если в меню уже есть откытые подменю, и клик происходит за пределами меню
             openedSubMenu.classList.remove('menu_active');   // закрываем открытое подменю
             openedSubMenu = null;   // откытых подменю больше нет
-         }
          };
-      }(m));
-   }
+      };
+   };
+      
 
 
 
